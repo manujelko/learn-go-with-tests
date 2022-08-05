4281f1a9c1c19b18901dc833e3a9f0d573fea0a4
Manuel Šarić <manujelko@gmail.com>
2022-08-05 17:46:19 +0100

    Refactor

--- roman_numerals/numerals.go
+++ roman_numerals/numerals.go
@@ -43,21 +43,15 @@ func ConvertToArabic(roman string) int {
 	for i := 0; i < len(roman); i++ {
 		symbol := roman[i]
 
-		// look ahead to next symbol if we can and, the currenty symbol is base 10 (only valid subtractors)
 		if couldBeSubtractive(i, symbol, roman) {
-			nextSymbol := roman[i+1]
-
-			// build the two character string
-			potentialNumber := string([]byte{symbol, nextSymbol})
-
-			if value := allRomanNumerals.ValueOf(potentialNumber); value != 0 {
+			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
 				total += value
 				i++ // move past this character too for the next loop
 			} else {
 				total++ // this is fishy...
 			}
 		} else {
-			total += allRomanNumerals.ValueOf(string([]byte{symbol}))
+			total += allRomanNumerals.ValueOf(symbol)
 		}
 	}
 	return total
@@ -69,7 +63,8 @@ func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
 
 type RomanNumerals []RomanNumeral
 
-func (r RomanNumerals) ValueOf(symbol string) int {
+func (r RomanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
 	for _, s := range r {
 		if s.Symbol == symbol {
 			return s.Value
