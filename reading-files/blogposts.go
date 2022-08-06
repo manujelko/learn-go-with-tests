02e0a2f52a2e7587391553a1439a93335476e495
Manuel Šarić <manujelko@gmail.com>
2022-08-06 08:35:54 +0100

    Reading files initial

---
+++ reading-files/blogposts.go
@@ -0,0 +1,15 @@
+package blogposts
+
+import (
+	"io/fs"
+	"testing/fstest"
+)
+
+func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
+	dir, _ := fs.ReadDir(fileSystem, ".")
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts
+}
---
+++ reading-files/post.go
@@ -0,0 +1,3 @@
+package blogposts
+
+type Post struct {}
