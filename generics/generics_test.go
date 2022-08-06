fd41bddbfddf3690c9e58c4718155412c195285d
Manuel Šarić <manujelko@gmail.com>
2022-08-06 07:57:41 +0100

    Generics - assert on integers

---
+++ generics/generics_test.go
@@ -0,0 +1,10 @@
+package generics
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+}
