a1163e6f383c13a53b054525b45731aa3458b93b
Manuel Šarić <manujelko@gmail.com>
2022-08-06 08:02:34 +0100

    Own test helpers with generics

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -7,4 +7,9 @@ func TestAssertFunctions(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
 	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
 }
